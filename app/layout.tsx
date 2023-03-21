85b49560fe6d9ef45f976cdda3640a76aa186d60
Fhloston Paradise <carson.franklin@outlook.com>
2023-03-20 22:29:18 -0700

    Changes2

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
             <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="h-auto rounded-lg bg-black p-3.5 lg:p-6">
                 {children}
+                <Analytics />
                 <AnalyticsWrapper />
                 <GAnalytics />
-                <Analytics />
               </div>
             </div>
 
